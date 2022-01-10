21f7d7678d62bddce056bf2df15d37b0e9f84875
mariamhap-py <95041043+mariamhap-py@users.noreply.github.com>
2022-01-10 17:12:01 +0100

    Update reconnaissance.py

--- src/reconnaissance.py
+++ src/reconnaissance.py
@@ -12,5 +12,26 @@ def lecture_modeles(chemin_dossier):
 
 
 def reconnaissance_chiffre(image, liste_modeles, S):
-    pass
+    #on binarise et localise l'image à comparer
+    im=image.binarisation(S)
+    im=im.localisation()
+    
+    #on stocke le taux de similitude le plus important 
+    sim_max =0
+    #on stocke l'entier dont le taux de similitude est le plus grand
+    entier_ress = 0
+    
+    #on parcourt la liste des images
+    for i in range (len(liste_modeles)):
+        #on redimensionne l'image à comparer avec celle de la liste
+        im=im.resize(liste_modeles[i].H, liste_modeles[i].W)
+        
+        #si le taux de similitude est supérieur, on l'affecte à sim_max
+        if sim_max<im.similitude(liste_modeles[i]) :
+            sim_max = im.similitude(liste_modeles[i])
+            #on stocke le chiffre correspondant à l'image la plus proche
+            entier_ress = i
+            
+    #on renvoie le chiffre trouvé
+    return entier_ress
 
